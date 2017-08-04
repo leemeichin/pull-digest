01159827f63bb0658efcd3ff13c1a8990ad1106d
Lee Machin <me@mrl.ee>
2017-08-04 18:48:14 +0300

    Correct filtering logic

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ const transformLabels = ({ status, labels }) => ({
 })
 
 const renderLine = (pr, filter) => ({ status, labels }) =>
-  filter && !labels
+  filter && (labels.length == 0 || labels.every(label => label !== filter))
     ? null
     : `<${pr.html_url}|${pr.title}> ${labels.join(', ')} (build: ${status})`
 
